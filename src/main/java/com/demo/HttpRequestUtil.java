3e587f1fc7167f928763da2b4d36a4aed1ac2e5e
tdp <tdp@tdpdeMacBook-Pro.local>
2018-05-29 09:24:41 +0800

    format

--- src/main/java/com/demo/HttpRequestUtil.java
+++ src/main/java/com/demo/HttpRequestUtil.java
@@ -5,18 +5,20 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpRequestUtil {
+
     /**
      * 从网络Url中下载文件
+     *
      * @param urlStr
      * @param fileName
      * @param savePath
      * @throws IOException
      */
-    public static void  downLoadFromUrl(String urlStr,String fileName,String savePath) throws IOException{
+    public static void downLoadFromUrl(String urlStr, String fileName, String savePath) throws IOException {
         URL url = new URL(urlStr);
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         //设置超时间为3秒
-        conn.setConnectTimeout(3*1000);
+        conn.setConnectTimeout(3 * 1000);
         //防止屏蔽程序抓取而返回403错误
         conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
         //得到输入流
@@ -26,34 +28,34 @@ public class HttpRequestUtil {
 
         //文件保存位置
         File saveDir = new File(savePath);
-        if(!saveDir.exists()){
+        if (!saveDir.exists()) {
             saveDir.mkdir();
         }
-        File file = new File(saveDir+File.separator+fileName);
+        File file = new File(saveDir + File.separator + fileName);
         FileOutputStream fos = new FileOutputStream(file);
         fos.write(getData);
-        if(fos!=null){
+        if (fos != null) {
             fos.close();
         }
-        if(inputStream!=null){
+        if (inputStream != null) {
             inputStream.close();
         }
-        System.out.println("info:"+url+" download success");
+        System.out.println("info:" + url + " download success");
     }
-
 
 
     /**
      * 从输入流中获取字节数组
+     *
      * @param inputStream
      * @return
      * @throws IOException
      */
-    public static  byte[] readInputStream(InputStream inputStream) throws IOException {
+    public static byte[] readInputStream(InputStream inputStream) throws IOException {
         byte[] buffer = new byte[1024];
         int len = 0;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        while((len = inputStream.read(buffer)) != -1) {
+        while ((len = inputStream.read(buffer)) != -1) {
             bos.write(buffer, 0, len);
         }
         bos.close();
--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -5,7 +5,7 @@ import com.qiniu.storage.BucketManager;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.model.FileInfo;
 import com.qiniu.util.Auth;
-import com.sun.deploy.net.*;
+import com.sun.deploy.net.URLEncoder;
 
 /**
  * Created by tdp on 2018/5/28.
@@ -13,10 +13,10 @@ import com.sun.deploy.net.*;
 public class Main {
 
     //AK/SK
-    static String accessKey = "1r1cmmWuTYaf-zW8WvqwnB_7mwXXXNOThKViA9_Z";
-    static String secretKey = "Xog5mzs9Ex25BXTC7hCK5TTLN-4UcE4ouv7dzvb-";
+    static String ACCESS_KEY = "1r1cmmWuTYaf-zW8WvqwnB_7mwXXXNOThKViA9_Z";
+    static String SECRET_KEY = "Xog5mzs9Ex25BXTC7hCK5TTLN-4UcE4ouv7dzvb-";
     //是否使用自定义域名下载对象
-    static boolean USE_CUSTOM_DOMAIN= true;
+    static boolean USE_CUSTOM_DOMAIN = true;
     //下载的文件保存路径
     static String SAVE_PATH = "/Users/tdp/IdeaProjects/qiniu-test/download";
 
@@ -24,22 +24,22 @@ public class Main {
         //构造一个带指定Zone对象的配置类
         Configuration cfg = new Configuration(Zone.huadong());
         String bucket = "pri-test";
-        Auth auth = Auth.create(accessKey, secretKey);
+        Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
         BucketManager bucketManager = new BucketManager(auth, cfg);
 
-        String[] domains =  bucketManager.domainList(bucket);
+        String[] domains = bucketManager.domainList(bucket);
         String domainName = "";
         if (domains == null || domains.length == 0) {
             System.out.println("zero domains");
             System.exit(1);
         }
-        for(int i=0 ; i< domains.length; i++){
+        for (int i = 0; i < domains.length; i++) {
             System.out.println(domains[i]);
 
-            if(domains[i].contains("clouddn.com") && !USE_CUSTOM_DOMAIN) {
+            if (domains[i].contains("clouddn.com") && !USE_CUSTOM_DOMAIN) {
                 domainName = domains[i];
                 break;
-            } else if (domains[i].contains("clouddn.com") && USE_CUSTOM_DOMAIN){
+            } else if (domains[i].contains("clouddn.com") && USE_CUSTOM_DOMAIN) {
                 continue;
             } else {
                 domainName = domains[i];
@@ -56,7 +56,7 @@ public class Main {
 
         //列举空间文件列表
         BucketManager.FileListIterator fileListIterator = bucketManager.createFileListIterator(bucket, prefix, limit, delimiter);
-        while(fileListIterator.hasNext()){
+        while (fileListIterator.hasNext()) {
             //处理获取的file list结果
             FileInfo[] items = fileListIterator.next();
             if (items == null || items.length == 0) {
@@ -64,19 +64,19 @@ public class Main {
                 break;
             }
             for (FileInfo item : items) {
-                String finalUrl = getObjectKey("http://"+ domainName,item.key);
-                System.out.println("Begin to download "+ finalUrl);
+                String finalUrl = getObjectKey("http://" + domainName, item.key);
+                System.out.println("Begin to download " + finalUrl);
                 HttpRequestUtil.downLoadFromUrl(finalUrl, item.key, SAVE_PATH);
-                System.out.println("End to download "+ finalUrl);
+                System.out.println("End to download " + finalUrl);
             }
         }
     }
 
     //获取对象的url， 带临时签名
-    public static String  getObjectKey(String domainOfBucket, String key) throws Exception {
+    public static String getObjectKey(String domainOfBucket, String key) throws Exception {
         String encodedFileName = URLEncoder.encode(key, "utf-8");
         String publicUrl = String.format("%s/%s", domainOfBucket, encodedFileName);
-        Auth auth = Auth.create(accessKey, secretKey);
+        Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
         long expireInSeconds = 3600;//1小时，可以自定义链接过期时间
         String finalUrl = auth.privateDownloadUrl(publicUrl, expireInSeconds);
         System.out.println(finalUrl);
